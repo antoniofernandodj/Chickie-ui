52db91c5513658c61da7ea6ca97b633638da7a24
Antonio Fernando <antoniofernandodj@outlook.com>
2026-05-11 11:44:46 -0300

    -

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'https://api.chiquitos.tech/api',
   NOTIFICATION_PATH: 'https://app.chiquitos.tech/sounds/notification.mp3',
   logLevel: 'error',
-  serverLogLevel: 'warn'
+  serverLogLevel: 'debug'
 };
