31e7ba93d4fc96c25f90ee3bda402ff61ecdd3a4
Antonio Fernando <antoniofernandodj@outlook.com>
2026-05-12 13:44:35 -0300

    -

--- src/app/features/admin/components/admin-config-pedido-tab.component.ts
+++ src/app/features/admin/components/admin-config-pedido-tab.component.ts
@@ -105,6 +105,10 @@ export class AdminConfigPedidoTabComponent {
     this.configPedidoError.set('');
     this.configPedidoService.getConfigPedido(uuid).subscribe({
       next: (config) => {
+
+        if (config.tipo_calculo == "MaisCaro") { config.tipo_calculo = "mais_caro"; }
+        if (config.tipo_calculo == "MediaPonderada") { config.tipo_calculo = "media_ponderada"; }
+
         this.configPedidoData.set(config);
         this.configPedidoForm.patchValue({ max_partes: Number(config.max_partes), tipo_calculo: config.tipo_calculo });
         this.configPedidoLoading.set(false);
